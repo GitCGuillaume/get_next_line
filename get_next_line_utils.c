b3165a8c7b3600eeca8bf4183d4005e4e34f605b
Guillaume CHOPIN <gchopin@e3r13p2.42.fr>
2020-05-15 14:42:59 +0200

    maj ft_strchr

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,22 +6,42 @@
 /*   By: gchopin <gchopin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/15 11:51:49 by gchopin           #+#    #+#             */
-/*   Updated: 2020/05/15 13:49:27 by gchopin          ###   ########.fr       */
+/*   Updated: 2020/05/15 14:41:28 by gchopin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strncpy(char *dest, char *src, int i)
+char	*ft_strcpy(char *dest, char *src, int i)
 {
 	unsigned int j;
 
 	j = 0;
-	while (src[i + j] != '\n' && src[i + j] != '\0' && BUFFER_SIZE > j)
+	while (src[i + j] != '\n' && src[i + j] != '\0')
 	{
 		dest[j] = src[i + j];
 		j++;
 	}
 	dest[j] = '\0';
 	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*located_c;
+	int		i;
+
+	located_c = (char *)s;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (located_c[i] == c)
+		{
+			return (&located_c[i]);
+		}
+		i++;
+	}
+	if (located_c[i] == c)
+		return (&located_c[i]);
+	return (0);
 }
