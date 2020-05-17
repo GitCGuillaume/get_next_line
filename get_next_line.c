0fba49b8e36cee6053639e1cc51440071fcab721
Guillaume CHOPIN <gchopin@e3r2p9.42.fr>
2020-05-17 19:26:35 +0200

    maj

--- get_next_line.c
+++ get_next_line.c
@@ -6,29 +6,40 @@
 /*   By: gchopin <gchopin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 14:02:07 by gchopin           #+#    #+#             */
-/*   Updated: 2020/05/16 15:39:18 by gchopin          ###   ########.fr       */
+/*   Updated: 2020/05/17 19:26:22 by gchopin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
-int		get_next_line(int fd, char **line)
+
+int		read_line(int fd, char **line)
 {
 	char	buff[BUFFER_SIZE + 1];
 	char	*tmp;
 	int		ret;
+
 	if (!(*line = malloc(sizeof(char))))
 		return (-1);
 	*line[0] = '\0';
 	while ((ret = read(fd, buff, BUFFER_SIZE)))
 	{
-		printf("line == %s\n", *line);
 		buff[ret] = '\0';
 		tmp = *line;
-		*line = ft_strjoin(tmp, buff);
+		if (!(*line = ft_strjoin(tmp, buff)))
+			return (-1);
 		free(tmp);
+			return (-1);
 		if (ft_strchr(buff, '\n'))
 			break;
 	}
-	return (0);
+	return (1);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	int		result;
+
+	result = read_line(fd, line);
+	return (1);
 }
