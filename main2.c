12167a1f98b683cfc5df7ec81377d3592e72325f
Guillaume CHOPIN <gchopin@e3r2p1.42.fr>
2020-06-02 21:57:48 +0200

    maj

--- main2.c
+++ main2.c
@@ -7,25 +7,22 @@ int main(void)
 	char 	*line;
 	int		out;
 	int		p[2];
-	char 	*str;
 	int		gnl_ret;
 
-	str = (char *)malloc(1000 * 1000);
-	*str = '\0';
-	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
-	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
-	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
 	out = dup(1);
 	pipe(p);
 	dup2(p[1], 1);
-	write(1, str, ft_len(str));
+	write(1, "abcd\nfk\njk", 11);
 	close(p[1]);
 	dup2(out, 1);
-	gnl_ret = get_next_line(p[0], &line);
-	printf("strcmp==%d\n", strcmp(line, str));
-	printf("ret==%d", gnl_ret);
+	gnl_ret = -1;
+	while (gnl_ret != 0)
+	{
+		gnl_ret = get_next_line(p[0], &line);
+		printf("strcmp==%s\n", line);
+		printf("ret==%d", gnl_ret);
+	}
 	//mt_assert(strcmp(line, str) == 0);
 	//mt_assert(gnl_ret == 0 || gnl_ret == 1);
 	free(line);
-	free(str);
 }
