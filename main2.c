9613253e423a445194bb5f28910676670be6b485
GitCGuillaume <34135668+GitCGuillaume@users.noreply.github.com>
2020-06-06 12:50:55 +0200

    maj depuis debian

--- main2.c
+++ main2.c
@@ -1,6 +1,5 @@
 //#include <project.h>
 #include <stdio.h>
-#include <string.h>
 #include "get_next_line.h"
 int main(void)
 {
@@ -12,19 +11,19 @@ int main(void)
 	out = dup(1);
 	pipe(p);
 	dup2(p[1], 1);
-	write(1, "abcd\nfk\njk", 11);
+	write(1, "az\ne", 5);
 	close(p[1]);
 	dup2(out, 1);
-	gnl_ret = -1;
+	gnl_ret = 1;
 	printf("\n------------\n");
 	while (gnl_ret != 0)
 	{
 		gnl_ret = get_next_line(p[0], &line);
 		printf("strcmp==%s", line);
-		printf("ret==%d\n", gnl_ret);
+		//printf("ret==%d\n", gnl_ret);
 	}
 	printf("\n------------\n");
-	//mt_assert(strcmp(line, str) == 0);
+	//mt_assert(strcmp(line, str) == 0)
 	//mt_assert(gnl_ret == 0 || gnl_ret == 1);
 	free(line);
 }
