3bacc034e9a6f15d9eb0103b831f61fc8db81c28
Guillaume CHOPIN <gchopin@e3r3p21.42.fr>
2020-06-09 10:30:24 +0200

    maj

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gchopin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/29 09:31:44 by gchopin           #+#    #+#             */
-/*   Updated: 2020/06/03 20:50:40 by gchopin          ###   ########.fr       */
+/*   Updated: 2020/06/09 10:29:09 by gchopin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int		clear_memory(char **str, int type)
 	return (-1);
 }
 
-int		length_line(char *str, int *res)
+int		len_n(char *str, int *res)
 {
 	size_t i;
 
@@ -73,11 +73,11 @@ int		get_last_n(char **line, char **m_line, int *res)
 		return (clear_memory(m_line, 0));
 	free(*line);
 	*line = mem_tmp;
-	if (!(mem_tmp = ft_substr(*m_line, length_line(*m_line, res), ft_len(*m_line))))
+	if (!(mem_tmp = ft_substr(*m_line, len_n(*m_line, res), ft_len(*m_line))))
 		return (clear_memory(m_line, 0));
 	free(*m_line);
 	*m_line = mem_tmp;
-	if ((*m_line)[0] == 0 ||*res == -1 ||*res == 0)
+	if ((*m_line)[0] == 0 || *res == -1 || *res == 0)
 		clear_memory(m_line, 1);
 	return (*res);
 }
