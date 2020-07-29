8ba583fb196b02e76dd62fecdc373726c256b2bb
Guillaume CHOPIN <gchopin@e2r1p9.42.fr>
2020-07-29 11:10:26 +0200

    add main2.c

--- main2.c
+++ main2.c
@@ -4,23 +4,23 @@
 int main(void)
 {
 	char 	*line;
-	int		out;
+	int	fd = 10000;
+	int		out = dup(fd);
 	int		p[2];
 	int		gnl_ret;
-
-	out = dup(1);
+	printf("open_max=%d\n", OPEN_MAX);
 	pipe(p);
-	dup2(p[1], 1);
-	write(1, "azsdfsfd\nskf\ndfert", 19);
+	dup2(p[1], fd);
+	write(fd, "azsdfsfd\nskf\ndfert", 19);
 	//write(1, NULL, 1);
+	dup2(out, fd);
 	close(p[1]);
-	dup2(out, 1);
 	gnl_ret = 1;
 	printf("\n------------\n");
 	//if (!(line = malloc(sizeof(char) * 10)))
 	//	return (0);
 	//while (gnl_ret != 0 && gnl_ret != -1)
-	while ((gnl_ret = get_next_line(p[0], &line)) > 0)
+	/*while ((gnl_ret = get_next_line(p[0], &line)) > 0)
 	{
 		//gnl_ret = get_next_line(p[0], &line);
 		printf("strcmp==%s", line);
@@ -29,16 +29,15 @@ int main(void)
 	}
 	printf("strcmp==%s", line);
 	printf("ret==%d\n", gnl_ret);
-	free(line);
-	//while (gnl_ret != 0 && gnl_ret != -1)
-	//{
-		/*gnl_ret = get_next_line(p[0], &line);
+	free(line);*/
+	/*while (gnl_ret != 0 && gnl_ret != -1)
+	{
+		gnl_ret = get_next_line(p[0], &line);
 		printf("strcmp==%s", line);
 		free(line);
 		printf("ret==%d\n", gnl_ret);
-		*/
-	//}
-
+	}*/
+	get_next_line(p[0], &line);
 	//free(line);
 	//printf("\n------------\n");
 	//mt_assert(strcmp(line, str) == 0)
